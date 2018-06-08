3055d086fa892c43f7bfef40e25b76428e105f31
CraftyJax <ajaff001@gmail.com>
2018-06-08 10:40:05 -0700

    updates and planck changes

--- common/testsuite.h
+++ common/testsuite.h
@@ -7556,6 +7556,75 @@ static void mavlink_test_high_latency2(uint8_t system_id, uint8_t component_id, 
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_planck_tag_meas(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_PLANCK_TAG_MEAS >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_planck_tag_meas_t packet_in = {
+        17.0,45.0,73.0,101.0,129.0,{ 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0 },409.0,437.0,465.0,493.0,521.0,549.0,577.0,605.0,633.0,963502248
+    };
+    mavlink_planck_tag_meas_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.cxy_x = packet_in.cxy_x;
+        packet1.cxy_y = packet_in.cxy_y;
+        packet1.x = packet_in.x;
+        packet1.y = packet_in.y;
+        packet1.z = packet_in.z;
+        packet1.x1 = packet_in.x1;
+        packet1.y1 = packet_in.y1;
+        packet1.x2 = packet_in.x2;
+        packet1.y2 = packet_in.y2;
+        packet1.x3 = packet_in.x3;
+        packet1.y3 = packet_in.y3;
+        packet1.x4 = packet_in.x4;
+        packet1.y4 = packet_in.y4;
+        packet1.tag_size = packet_in.tag_size;
+        packet1.grab_time_ms = packet_in.grab_time_ms;
+        
+        mav_array_memcpy(packet1.rot_CF_TF, packet_in.rot_CF_TF, sizeof(float)*9);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_PLANCK_TAG_MEAS_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_PLANCK_TAG_MEAS_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_planck_tag_meas_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_planck_tag_meas_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_planck_tag_meas_pack(system_id, component_id, &msg , packet1.cxy_x , packet1.cxy_y , packet1.x , packet1.y , packet1.z , packet1.rot_CF_TF , packet1.x1 , packet1.y1 , packet1.x2 , packet1.y2 , packet1.x3 , packet1.y3 , packet1.x4 , packet1.y4 , packet1.tag_size , packet1.grab_time_ms );
+    mavlink_msg_planck_tag_meas_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_planck_tag_meas_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.cxy_x , packet1.cxy_y , packet1.x , packet1.y , packet1.z , packet1.rot_CF_TF , packet1.x1 , packet1.y1 , packet1.x2 , packet1.y2 , packet1.x3 , packet1.y3 , packet1.x4 , packet1.y4 , packet1.tag_size , packet1.grab_time_ms );
+    mavlink_msg_planck_tag_meas_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_planck_tag_meas_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_planck_tag_meas_send(MAVLINK_COMM_1 , packet1.cxy_x , packet1.cxy_y , packet1.x , packet1.y , packet1.z , packet1.rot_CF_TF , packet1.x1 , packet1.y1 , packet1.x2 , packet1.y2 , packet1.x3 , packet1.y3 , packet1.x4 , packet1.y4 , packet1.tag_size , packet1.grab_time_ms );
+    mavlink_msg_planck_tag_meas_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_vibration(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -10096,6 +10165,7 @@ static void mavlink_test_common(uint8_t system_id, uint8_t component_id, mavlink
     mavlink_test_gps_rtcm_data(system_id, component_id, last_msg);
     mavlink_test_high_latency(system_id, component_id, last_msg);
     mavlink_test_high_latency2(system_id, component_id, last_msg);
+    mavlink_test_planck_tag_meas(system_id, component_id, last_msg);
     mavlink_test_vibration(system_id, component_id, last_msg);
     mavlink_test_home_position(system_id, component_id, last_msg);
     mavlink_test_set_home_position(system_id, component_id, last_msg);
