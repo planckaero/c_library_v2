e6b4054f4c62cf1344375b50ec817aa0781848bd
aubrey <clausse.aubrey@gmail.com>
2022-10-12 19:07:32 -0700

    mavlink commit 0f78779f

--- planckAero/testsuite.h
+++ planckAero/testsuite.h
@@ -539,21 +539,21 @@ static void mavlink_test_planck_landing_tag_estimate_gps(uint8_t system_id, uint
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
-static void mavlink_test_planck_landing_tag_measurement(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+static void mavlink_test_planck_landing_target_measurement(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
     mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_PLANCK_LANDING_TAG_MEASUREMENT >= 256) {
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_PLANCK_LANDING_TARGET_MEASUREMENT >= 256) {
             return;
         }
 #endif
     mavlink_message_t msg;
         uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
         uint16_t i;
-    mavlink_planck_landing_tag_measurement_t packet_in = {
-        93372036854775807ULL,963497880,101.0,129.0,157.0,{ 185.0, 186.0, 187.0, 188.0, 189.0, 190.0 },{ 353.0, 354.0, 355.0, 356.0 },{ 465.0, 466.0, 467.0, 468.0, 469.0, 470.0, 471.0, 472.0, 473.0, 474.0 },61,128,195,6
+    mavlink_planck_landing_target_measurement_t packet_in = {
+        93372036854775807ULL,963497880,101.0,129.0,157.0,{ 185.0, 186.0, 187.0, 188.0, 189.0, 190.0 },{ 353.0, 354.0, 355.0, 356.0 },{ 465.0, 466.0, 467.0, 468.0, 469.0, 470.0, 471.0, 472.0, 473.0, 474.0 },61,128,195,6,73
     };
-    mavlink_planck_landing_tag_measurement_t packet1, packet2;
+    mavlink_planck_landing_target_measurement_t packet1, packet2;
         memset(&packet1, 0, sizeof(packet1));
         packet1.ap_timestamp_us = packet_in.ap_timestamp_us;
         packet1.age = packet_in.age;
@@ -564,6 +564,7 @@ static void mavlink_test_planck_landing_tag_measurement(uint8_t system_id, uint8
         packet1.target_component = packet_in.target_component;
         packet1.camera = packet_in.camera;
         packet1.frame = packet_in.frame;
+        packet1.target_index = packet_in.target_index;
         
         mav_array_memcpy(packet1.pos_covariance, packet_in.pos_covariance, sizeof(float)*6);
         mav_array_memcpy(packet1.quaternion, packet_in.quaternion, sizeof(float)*4);
@@ -572,22 +573,22 @@ static void mavlink_test_planck_landing_tag_measurement(uint8_t system_id, uint8
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
         if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
            // cope with extensions
-           memset(MAVLINK_MSG_ID_PLANCK_LANDING_TAG_MEASUREMENT_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_PLANCK_LANDING_TAG_MEASUREMENT_MIN_LEN);
+           memset(MAVLINK_MSG_ID_PLANCK_LANDING_TARGET_MEASUREMENT_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_PLANCK_LANDING_TARGET_MEASUREMENT_MIN_LEN);
         }
 #endif
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_planck_landing_tag_measurement_encode(system_id, component_id, &msg, &packet1);
-    mavlink_msg_planck_landing_tag_measurement_decode(&msg, &packet2);
+    mavlink_msg_planck_landing_target_measurement_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_planck_landing_target_measurement_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_planck_landing_tag_measurement_pack(system_id, component_id, &msg , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
-    mavlink_msg_planck_landing_tag_measurement_decode(&msg, &packet2);
+    mavlink_msg_planck_landing_target_measurement_pack(system_id, component_id, &msg , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.target_index , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
+    mavlink_msg_planck_landing_target_measurement_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_planck_landing_tag_measurement_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
-    mavlink_msg_planck_landing_tag_measurement_decode(&msg, &packet2);
+    mavlink_msg_planck_landing_target_measurement_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.target_index , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
+    mavlink_msg_planck_landing_target_measurement_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
@@ -595,12 +596,12 @@ static void mavlink_test_planck_landing_tag_measurement(uint8_t system_id, uint8
         for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
-    mavlink_msg_planck_landing_tag_measurement_decode(last_msg, &packet2);
+    mavlink_msg_planck_landing_target_measurement_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_planck_landing_tag_measurement_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
-    mavlink_msg_planck_landing_tag_measurement_decode(last_msg, &packet2);
+    mavlink_msg_planck_landing_target_measurement_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.ap_timestamp_us , packet1.age , packet1.camera , packet1.frame , packet1.target_index , packet1.x , packet1.y , packet1.z , packet1.pos_covariance , packet1.quaternion , packet1.quat_covariance );
+    mavlink_msg_planck_landing_target_measurement_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
@@ -614,7 +615,7 @@ static void mavlink_test_planckAero(uint8_t system_id, uint8_t component_id, mav
     mavlink_test_planck_landing_tag_estimate_ned(system_id, component_id, last_msg);
     mavlink_test_planck_landing_tag_measurement_body(system_id, component_id, last_msg);
     mavlink_test_planck_landing_tag_estimate_gps(system_id, component_id, last_msg);
-    mavlink_test_planck_landing_tag_measurement(system_id, component_id, last_msg);
+    mavlink_test_planck_landing_target_measurement(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
